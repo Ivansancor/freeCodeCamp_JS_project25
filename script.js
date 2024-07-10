141ccfd4203dbaa69f625f8ee4d60368bda3d342
Ivansancor <ivanvaldeolivo95@gmail.com>
2024-07-10 02:40:27 +0200

    implemented basic endpoint api search functionality with try catch block

--- script.js
+++ script.js
@@ -14,13 +14,37 @@ const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
 
-const searchPokemon = () => {
-    if (searchInput.value === "") {
-        alert("Please enter a Pokemon name or ID")
-    } else {
-        console.log(searchInput.value);
+const allPokeEndpoints = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon"
+const specificPokeEndpoint = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/`
+
+const validateInput = input => {
+    let validatedInput = input.toLowerCase().trim();
+    if (input.includes("♀")){
+        validatedInput = validatedInput.replace("♀", "-f");
     }
-}
+    if (input.includes("♂")){
+        validatedInput = validatedInput.replace("♂", "-m");
+    }
+    validatedInput = validatedInput.replace(/[\W_]/gi, "-").replace(/^-*|-*$/gi, "").replace(/-{2,}/gi, "-");
+    return validatedInput;
+};
+
+const searchPokemon = async () => {
+    if (searchInput.value === "") {
+        alert("Please enter a Pokémon name or ID")
+    } else {
+        const validatedInput = validateInput(searchInput.value);
+        try {
+            const res = await fetch(specificPokeEndpoint + validatedInput);
+            const data = await res.json();
+            console.log(data);
+        }
+        catch (err) {
+            alert("Pokémon not found");
+            console.log("There was an error: \n" + err);
+        }
+    }
+};
 
 searchInput.addEventListener("keyup", e => {
     if (e.key === "Enter") {
