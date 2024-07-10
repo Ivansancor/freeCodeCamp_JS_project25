8794f95bcb5102f3a9e0f2d5a87b9afe5e608f18
Ivansancor <ivanvaldeolivo95@gmail.com>
2024-07-10 06:55:43 +0200

    finalized app functionality as per fCC's guidelines

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
 const pokemonWeight = document.getElementById("weight");
 const pokemonHeight = document.getElementById("height");
+const spriteContainer = document.getElementById("sprite-container");
 const pokemonTypes = document.getElementById("types");
 
 const hp = document.getElementById("hp");
@@ -14,7 +15,6 @@ const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
 
-const allPokeEndpoints = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon"
 const specificPokeEndpoint = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/`
 
 const validateInput = input => {
@@ -32,30 +32,74 @@ const validateInput = input => {
 
 const searchPokemon = async () => {
     if (searchInput.value === "") {
-        alert("Please enter a Pokémon name or ID")
+        alert("Please enter a Pokémon name or ID");
     } else {
         const validatedInput = validateInput(searchInput.value);
+        searchInput.value = "";
         try {
             const res = await fetch(specificPokeEndpoint + validatedInput);
             const data = await res.json();
-            const {name, id, weight, height, types, sprites} = data;
-            const defaultSpriteUrl = sprites["front_default"];
-            let typesArr = [];
-            for(let type of types){
-                typesArr.push(type["type"].name);
-            }
-            console.log(data);
-            console.log(typesArr);
-            
+            const {name, id, weight, height, types, sprites, stats} = data;
+
+
+            //displaying basic info
             pokemonName.textContent = name;
             pokemonId.textContent = "#" + id;
             pokemonWeight.textContent = "Weight: " + weight;
             pokemonHeight.textContent = "Height: " + height;
-            pokemonTypes.textContent = typesArr.join(", ");
 
-            const pokemonImg = document.createElement("img");
-            pokemonImg.innerText = `src="${defaultSpriteUrl}" alt="${name}-image"`;
-            pokemonTypes.parentElement.insertBefore(pokemonImg, pokemonTypes);
+
+            //defining and displaying types
+            const typeColorMap = {
+                normal: '#A8A77A',
+                fire: '#EE8130',
+                water: '#6390F0',
+                electric: '#F7D02C',
+                grass: '#7AC74C',
+                ice: '#96D9D6',
+                fighting: '#C22E28',
+                poison: '#A33EA1',
+                ground: '#E2BF65',
+                flying: '#A98FF3',
+                psychic: '#F95587',
+                bug: '#A6B91A',
+                rock: '#B6A136',
+                ghost: '#735797',
+                dragon: '#6F35FC',
+                dark: '#705746',
+                steel: '#B7B7CE',
+                fairy: '#D685AD',
+            };
+
+            let typesArr = [];
+            for(let type of types){
+                typesArr.push(type["type"].name);
+            }  
+
+            let typesHTML = "";
+            for(let element of typesArr){
+                typesHTML += `<span style="background-color: ${typeColorMap[element]};">${element}</span>`;
+            }
+            pokemonTypes.innerHTML = typesHTML;
+
+
+            //defining and displaying sprite
+            const defaultSpriteUrl = sprites["front_default"];
+            spriteContainer.innerHTML = `<img src="${defaultSpriteUrl}" alt="${name}-front-sprite">`
+
+
+            //defining and displaying stats
+            const statsObj = {}
+            for(let stat of stats){
+                statsObj[stat["stat"].name] = stat["base_stat"];
+            }
+
+            hp.textContent = statsObj.hp;
+            attack.textContent = statsObj.attack;
+            defense.textContent = statsObj.defense;
+            specialAttack.textContent = statsObj["special-attack"];
+            specialDefense.textContent = statsObj["special-defense"];
+            speed.textContent = statsObj.speed;
         }
         catch (err) {
             alert("Pokémon not found");
@@ -71,4 +115,4 @@ searchInput.addEventListener("keyup", e => {
 }
 );
 
-searchButton.addEventListener("click", searchPokemon)
+searchButton.addEventListener("click", searchPokemon);
